b248fa9e2e88a7f27d056f02fcf131a90e1720d6
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2021-02-07 12:00:51 -0800

    have my server running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
 // build your server here
+const express = require("express");
+const helmet = require("helmet")
+
+const server = express();
+
+server.use(express.json())
+server.use(helmet())
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // start your server here
+const server = require("./api/server");
+
+const PORT = process.env.PORT || 2700;
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+})
